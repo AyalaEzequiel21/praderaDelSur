bab3462fc1685485b2b87eb41a4b60edd4b25f49
Checho <ezequielayala.c@gmail.com>
2023-06-10 22:45:24 -0300

    tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,9 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|svg)$': 'identity-obj-proxy',
-    },
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-      },
-    transformIgnorePatterns: [
-        '/node_modules/(?!.*\\.(js|jsx|ts|tsx)$)', // Ignorar archivos binarios en node_modules
-        '^.+\\.module\\.(css|sass|scss)$', // Ignorar archivos CSS/Sass modules
-    ],
-  };
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test|tests).[tj]s?(x)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+}
   
